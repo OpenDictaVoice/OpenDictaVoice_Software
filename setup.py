3dd205eacc52670eddff0f4ce1fe30cdb2a2b3d4
r0b3rt[0] <nullepart@rien.com>
2021-04-04 10:34:58 +0200

    need to check the setup files

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='OpenDictaVoice',
       version='1.0',
       description='A python3 program to do voice dictation',
-      author='Francois Schwarzentruber and Victor Vague',
-      author_email='frippon@supersonique.pouet',
-      url='https://www.gitlab.org/lien_vers_le_repo',
+      author='Francois Schwarzentruber, R0b3rt[0] and Victor Vague',
+      author_email='opendictavoice@outlook.com',
+      url='https://github.com/OpenDictaVoice/OpenDictaVoice_Software',
       packages=['opendictavoice_app',
                 'opendictavoice_app.opendictavoice_modules'],
-      py_modules=['opendictavoice_app.opendictavoice_main',
-                  'opendictavoice_app.opendictavoice_modules.audio_manager',
-                  'opendictavoice_app.opendictavoice_modules.builded_GUI',
-                  'opendictavoice_app.opendictavoice_modules.fifo',
-                  'opendictavoice_app.opendictavoice_modules.keyboard_listener',
-                  'opendictavoice_app.opendictavoice_modules.main_app',
-                  'opendictavoice_app.opendictavoice_modules.voice_recognizer'],
+#      py_modules=['opendictavoice_app.opendictavoice_main',
+#                  'opendictavoice_app.opendictavoice_modules.audio_manager',
+#                  'opendictavoice_app.opendictavoice_modules.builded_GUI',
+#                  'opendictavoice_app.opendictavoice_modules.fifo',
+#                  'opendictavoice_app.opendictavoice_modules.keyboard_listener',
+#                  'opendictavoice_app.opendictavoice_modules.main_app',
+#                  'opendictavoice_app.opendictavoice_modules.voice_recognizer'],
       data_files=[('./opendictavoices_app/resources', '')],
-      requires=['PyAudio', 'pynput', 'pythonxlib', 'six', 'SpeechRecognition']
+      install_requires=['PyAudio', 'pynput', 'pythonxlib', 'six', 'SpeechRecognition'],
+      license="GNU General Public License v3.0"
 )
